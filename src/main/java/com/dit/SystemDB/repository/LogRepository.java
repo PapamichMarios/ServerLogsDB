93d3d6b848ca7be4a96a7da10ef1363e73adab26
PapamichMarios <papamichalopoulos.m@gmail.com>
2019-11-18 23:15:34 +0200

    Added a missing annotation

--- src/main/java/com/dit/SystemDB/repository/LogRepository.java
+++ src/main/java/com/dit/SystemDB/repository/LogRepository.java
@@ -4,13 +4,16 @@ import com.dit.SystemDB.model.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.RepositoryDefinition;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+@Repository
 public interface LogRepository extends JpaRepository<Log, Long> {
 
     @Query(nativeQuery = true, value="SELECT time_stamp, total FROM log_db.function1(:time1, :time2)")
